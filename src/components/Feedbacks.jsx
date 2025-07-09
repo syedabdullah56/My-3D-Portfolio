ebb97e52ae463253ef7da6f457f6be805c72c66f
syedabdullah56 <syedabdullah8750@gmail.com>
2025-07-09 14:15:55 +0500

    Made the feedback Section Successfully

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
+
+const FeedbackCard = ({ index, name, designation, company, image, testimonial }) => {
+  return (
+    <motion.div
+      variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+      className="bg-[#1d1836] p-6 rounded-2xl sm:w-[360px] w-full shadow-lg"
+    >
+      <p className="text-white font-black text-[40px] mb-4">“</p>
+      <p className="text-white text-[16px] leading-[28px]">{testimonial}</p>
+
+      <div className="mt-6 flex items-center justify-between">
+        <div className="flex flex-col">
+          <p className="text-white font-semibold text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
+          </p>
+          <p className="text-sm text-[#aaa6c3]">
+            {designation} at {company}
+          </p>
+        </div>
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className="w-12 h-12 rounded-full object-cover border-2 border-white"
+        />
+      </div>
+    </motion.div>
+  );
+};
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
-}
+    <div className="mt-16 bg-[#100d25] rounded-[20px]">
+      <div className={`${styles.padding} bg-[#151030] rounded-t-2xl`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials</h2>
+        </motion.div>
+      </div>
 
-export default Feedbacks
+      <div
+        className={`${styles.paddingX} py-10 flex flex-wrap justify-center gap-6 sm:gap-10 `}
+      >
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial}  />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Feedbacks, 'feedbacks');
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,7 +10,10 @@ const SectionWrapper = (Component,idName) =>
                 initial="hidden"
                 whileInView="show"
                 viewport={{ once: false, amount: 0.25 }}   //I have added once false and amount 0.25 to make sure the section is animated when it comes into view
-                className={` ${styles.padding} max-w-7xl mx-auto relative left-[2vh] z-0 `}
+                // className={` ${styles.padding} max-w-7xl mx-auto  relative  z-0  sm:left-[2vh] md:left-[4vh] lg:left-[9vh]`}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0 left-[1vh] sm:left-[2vh] md:left-[4vh] lg:left-[9vh]`}
+
+
                 >
                 <span className="hash-span" id={idName}>
                     &nbsp;
